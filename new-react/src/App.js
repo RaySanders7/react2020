f65d0d1faf39673f5131af171abe4c43ec6db9c0
Ray Sanders <raymond.sanders@gmail.com>
2020-07-09 13:44:37 -0500

    Quick and dirty static site

--- new-react/src/App.js
+++ new-react/src/App.js
@@ -2,23 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>HR System</p>
       </header>
+      <div className="data">
+        <div className="table-header">
+          <div className='employee-id'>Employee Id</div>
+          <div className='first-name'>First Name</div>
+          <div className='last-name'>Last Name</div>
+          <div className='email'>Email</div>
+          <div className='gender'>Gender</div>
+          <div className='race'>Race</div>
+          <div className='age'>Age</div>
+          <div className='job-title'>Job Title</div>
+          <div className='department'>Department</div>
+        </div>
+        <div className="table-data">
+          <div className='employee-id'>1</div>
+          <div className='first-name'>Chuck</div>
+          <div className='last-name'>Daubeny</div>
+          <div className='email'>cdaubeny0@vistaprint.com</div>
+          <div className='gender'>Male</div>
+          <div className='race'>Prefer Not to Answer</div>
+          <div className='age'>7/19/1984</div>
+          <div className='job-title'>Senior Editor</div>
+          <div className='department'>Marketing</div>
+        </div>
+      </div>
     </div>
   );
 }
