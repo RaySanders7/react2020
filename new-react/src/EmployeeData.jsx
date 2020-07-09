c8d2651d5acef858c2deeb4b0b20389ac8edfaaf
Ray Sanders <raymond.sanders@gmail.com>
2020-07-09 17:26:00 -0500

    Wire up SearchBar filtering

--- new-react/src/EmployeeData.jsx
+++ new-react/src/EmployeeData.jsx
@@ -7,14 +7,17 @@ import tempData from './data/tempData.json'
 
 const EmployeeData = () => {
   const [employeeData, setEmployeeData] = useState([]);
+  const [filterText, setFilterText] = useState('');
 
   useEffect(() => {
     setEmployeeData(tempData);
   }, []);
 
+  const onFilterTextChange = text => setFilterText(text);
+
   return (
     <div className="data">
-      <SearchBar />
+      <SearchBar filterText={filterText} onFilterTextChange={onFilterTextChange} />
       <TableHeader />
       {
         employeeData.map(e => <TableDataItem key={e.employeeId} data={e} />)
--- new-react/src/SearchBar.jsx
+++ new-react/src/SearchBar.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './SearchBar.css';
+import { useEffect } from 'react';
 
-const SearchBar = () => (
+const SearchBar = ({ filterText, onFilterTextChange }) => {
+  const handleFilterTextChange = (e) => onFilterTextChange(e.target.value);
+ 
+  return (
   <form className="search-bar">
-    <input type="text" placeholder="Search..." />
+    <input type="text" placeholder="Search..." value={filterText} onChange={handleFilterTextChange} />
   </form>
-);
+)};
 
 export default SearchBar;
