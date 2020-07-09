356ad994d201a15e4edc1cabc1d79a5645683cb1
Ray Sanders <raymond.sanders@gmail.com>
2020-07-09 14:32:01 -0500

    Add a search bar component

--- new-react/src/EmployeeData.jsx
+++ new-react/src/EmployeeData.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import TableHeader from './TableHeader';
 import TableDataItem from './TableDataItem';
+import SearchBar from './SearchBar';
 
 // TODO: State Lives Here
 const EmployeeData = () => (
   <div className="data">
+    <SearchBar />
     <TableHeader />
     <TableDataItem />
   </div>
---
+++ new-react/src/SearchBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './SearchBar.css';
+
+const SearchBar = () => (
+  <form className="search-bar">
+    <input type="text" placeholder="Search..." />
+  </form>
+);
+
+export default SearchBar;
